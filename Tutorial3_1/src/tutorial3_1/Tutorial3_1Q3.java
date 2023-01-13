c471084d82a1e3124521ea81ad8062f10170543c
Baltoch <balthazar.lebreton@gmail.com>
2023-01-13 12:42:32 +0000

    Tutorial 3.1 Q3 import

---
+++ Tutorial3_1/src/tutorial3_1/Tutorial3_1Q3.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial3_1;
+
+/**
+ *
+ * @author balth
+ */
+
+/**
+ * @hidden 
+ * Write a class named Employee that has the following fields: 
+ * •	name. The name field references a String object that holds the employee’s name. 
+ * •	idNumber. The idNumber is an int variable that holds the employee’s ID number.
+ * •	department. The department field references a String object that holds the name of the 
+ *      department where the employee works. 
+ * •	position. The position field references a String object that holds the employee’s job title. 
+ * The class should have the following constructors: 
+ * •	A constructor that accepts the following values as arguments and assign them to the 
+ *      appropriate fields: employee’s name, employee’s ID number, department, and position. 
+ * •	A constructor that accepts the following values as arguments and assign them to the 
+ *      appropriate fields: employee’s name and ID number. The department and position fields 
+ *      should be assigned an empty string ("").
+ * Write appropriate accessor and mutator methods. Create at least five Employee objects using 
+ * the above constructors and display the data for each employee on the screen
+ * 
+ */
+public class Tutorial3_1Q3 {
+    
+}
