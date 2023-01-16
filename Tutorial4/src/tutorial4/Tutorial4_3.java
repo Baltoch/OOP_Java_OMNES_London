23bb05bef259a7afab2245aca1c5315be093d09a
Baltoch <balthazar.lebreton@gmail.com>
2023-01-16 12:45:02 +0000

    Tutorial 4.3 import

---
+++ Tutorial4/src/tutorial4/Tutorial4_3.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial4;
+
+/**
+ *
+ * @author balth
+ */
+
+/**
+ * @hidden
+ * Write a class named RetailItem that holds data about an item in a retail store. The class  should 
+ * have the following fields: 
+ * 
+ *      description: The description field references a String object that holds a brief description 
+ *      of the item.  
+ * 
+ *      unitsOnHand: The unitsOnHand field is an int variable that holds the number of units 
+ *      currently in inventory.  
+ * 
+ *      price: The price field is a double that holds the item’s retail price.
+ * 
+ * Write a constructor that accepts arguments for each field and accessor methods that return 
+ * the values in these fields. Once you have written the class, write a separate program that 
+ * creates three RetailItem objects and stores the following data in them:  
+ * 
+ *                       Description       Units on Hand      Price
+ *          Item #1      Jacket                  12           59.95
+ *          Item #2      Designer Jeans          40           34.95
+ *          Item #3      Shirt                   20           24.95
+ * 
+ * 
+ * Write a CashRegister class that can be used with the RetailItem class. The CashRegister class 
+ * should simulate the sale of a retail item. It should have a constructor that accepts a RetailItem
+ * object as an argument. The constructor should also accept an integer that represents the quantity
+ * of items being purchased. In addition, the class should have the following methods: 
+ * 
+ *      getSubtotal method should return the subtotal of the sale, which is the quantity multiplied
+ *      by the price. This method must get the price from the RetailItem object that was passed as
+ *      an argument to the constructor.  
+ * 
+ *      getTax method should return the amount of sales tax on the purchase. The sales tax rate is
+ *      6 percent of a retail sale.  
+ * 
+ *      getTotal method should return the total of the sale, which is the subtotal plus the 
+ *      sales tax. Demonstrate the class in a program that asks the user for the quantity of 
+ *      items being purchased, and then displays the sale’s subtotal, amount of sales tax, and total. 
+ * 
+ */
+public class Tutorial4_3 {
+    
+}
+
+class RetailItem {
+    
+}
