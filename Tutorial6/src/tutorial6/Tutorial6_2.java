a88bf62bd0e219ddf65d266c386d45f3fdb4e3d2
Baltoch <balthazar.lebreton@gmail.com>
2023-01-19 14:45:07 +0000

    Tutorial 6.2 import

---
+++ Tutorial6/src/tutorial6/Tutorial6_2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial6;
+
+/**
+ *
+ * @author balth
+ */
+public class Tutorial6_2 {
+    
+}
